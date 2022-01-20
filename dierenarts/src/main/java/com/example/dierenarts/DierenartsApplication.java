175590edbc0b6e5411d3a213183ea580de103548
emrysavalon <tanja@hunen.nl>
2022-01-20 17:44:06 +0100

    Initial project setup

---
+++ dierenarts/src/main/java/com/example/dierenarts/DierenartsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dierenarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DierenartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DierenartsApplication.class, args);
+    }
+
+}
---
+++ dierenarts/src/test/java/com/example/dierenarts/DierenartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dierenarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DierenartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
