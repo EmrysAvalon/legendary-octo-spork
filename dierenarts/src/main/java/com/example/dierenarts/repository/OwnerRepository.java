a08ac82f7525b79575ada23a924de254290aac7d
EmrysAvalon <tanja@hunen.nl>
2022-02-04 23:03:25 +0100

    added missing repository annotations.

--- dierenarts/src/main/java/com/example/dierenarts/repository/OwnerRepository.java
+++ dierenarts/src/main/java/com/example/dierenarts/repository/OwnerRepository.java
@@ -1,9 +1,10 @@
 package com.example.dierenarts.repository;
 
 import com.example.dierenarts.model.Owner;
-import com.example.dierenarts.model.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
     Iterable<Owner> findAllByName(String name);
--- dierenarts/src/main/java/com/example/dierenarts/repository/PetRepository.java
+++ dierenarts/src/main/java/com/example/dierenarts/repository/PetRepository.java
@@ -3,7 +3,9 @@ package com.example.dierenarts.repository;
 import com.example.dierenarts.model.Owner;
 import com.example.dierenarts.model.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
     Iterable<Pet> findAllByName(String name);
